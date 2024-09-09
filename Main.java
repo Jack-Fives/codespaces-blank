41fc6801b22b8d79c1045abc82a79c2abdbc61a8
Jack-Fives <jackf58@nycstudents.net>
2024-09-09 02:07:57 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Do my homework for me.");
+        System.out.println("I'm sorry Dave, I can't do that.");
+    }
+}
